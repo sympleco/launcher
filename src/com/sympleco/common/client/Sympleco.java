fe3e28938eaf07465daad92c1f53a1c30ff41537
eklavyamishra <eklavyamishra@192.168.0.102>
2011-01-09 19:12:32 -0800

    Adding a Gadget

--- src/com/sympleco/common/client/Sympleco.java
+++ src/com/sympleco/common/client/Sympleco.java
@@ -1,41 +1,9 @@
 package com.sympleco.common.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.smartgwt.client.widgets.layout.HStack;
-import com.smartgwt.client.widgets.layout.VStack;
-import com.smartgwt.client.widgets.Label;
 import com.smartgwt.client.widgets.Canvas;
-import com.smartgwt.client.widgets.toolbar.ToolStrip;
-import com.smartgwt.client.widgets.form.DynamicForm;
-import com.smartgwt.client.widgets.IButton;
-import com.smartgwt.client.widgets.ImgButton;
-import com.smartgwt.client.widgets.toolbar.ToolStripButton;
-import com.smartgwt.client.widgets.Img;
-import com.smartgwt.client.widgets.toolbar.ToolStripSeparator;
-import com.smartgwt.client.widgets.toolbar.ToolStripMenuButton;
-import com.smartgwt.client.widgets.toolbar.ToolStripSpacer;
-import com.smartgwt.client.widgets.layout.HLayout;
-import com.smartgwt.client.widgets.layout.VLayout;
-import com.smartgwt.client.widgets.tile.TileLayout;
-import com.smartgwt.client.widgets.grid.ListGrid;
-import com.smartgwt.client.widgets.HTMLPane;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.InlineLabel;
-import com.google.gwt.user.client.ui.InlineHTML;
-import com.smartgwt.client.widgets.HTMLFlow;
 import com.smartgwt.client.widgets.toolbar.ToolStripResizer;
-import com.smartgwt.client.widgets.RichTextEditor;
-import com.smartgwt.client.widgets.form.SearchForm;
-import com.smartgwt.client.types.FormMethod;
-import com.smartgwt.client.widgets.form.fields.TextAreaItem;
-import com.smartgwt.client.widgets.form.fields.TextItem;
-import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Grid;
 
 /**
@@ -54,7 +22,5 @@ public class Sympleco implements EntryPoint {
 //		InlineHTML nlnhtmlSympleSolutions = new InlineHTML("Symple Solutions");
 		grid.setWidget(0, 0, new ToolStripResizer());
 		grid.setWidget(0, 1, cv);
-		
 	}
-
 }
