4fa2af21bd4dd104e14192a2ecb3811f9b67911c
eklavyamishra <eklavyamishra@192.168.0.100>
2011-01-17 19:07:43 -0800

    comiit the UI

--- src/com/sympleco/common/client/TimeSheetUI.java
+++ src/com/sympleco/common/client/TimeSheetUI.java
@@ -37,6 +37,7 @@ import com.smartgwt.client.widgets.form.fields.TextAreaItem;
 import com.smartgwt.client.widgets.form.fields.TextItem;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Grid;
+import com.smartgwt.client.widgets.layout.SectionStack;
 
 public class TimeSheetUI {
 	/**
@@ -48,60 +49,9 @@ public class TimeSheetUI {
 		VLayout layout = new VLayout();
 		layout.setAutoHeight();
 		layout.setSize("601px", "417px");
-		HLayout hLayout_2 = new HLayout();
-		hLayout_2.setSize("882px", "16px");
 		
-		HTMLFlow flwtheSympleCompany = new HTMLFlow("<bold>The Symple Company </bold>");
-		hLayout_2.addMember(flwtheSympleCompany);
-		layout.addMember(hLayout_2);
-
-		ToolStrip toolStrip = new ToolStrip();
-		toolStrip.setSize("882px", "24px");
-
-		ToolStripButton toolStripButton_about = new ToolStripButton("About");
-//		toolStripButton_about.setWidth("");
-		toolStrip.addButton(toolStripButton_about);
-
-		ToolStripSeparator toolStripSeparator = new ToolStripSeparator();
-		toolStrip.addMember(toolStripSeparator);
-
-		ToolStripButton toolStripButton_product = new ToolStripButton("Product");
-//		toolStripButton_product.setWidth("");
-		toolStrip.addButton(toolStripButton_product);
-
-		ToolStripSeparator toolStripSeparator_1 = new ToolStripSeparator();
-		toolStrip.addMember(toolStripSeparator_1);
-
-		ToolStripButton toolStripButton_partner = new ToolStripButton("Partner");
-		toolStrip.addButton(toolStripButton_partner);
-
-		ToolStripSeparator toolStripSeparator_3 = new ToolStripSeparator();
-		toolStrip.addMember(toolStripSeparator_3);
-
-		ToolStripButton toolStripButton_pricing = new ToolStripButton("Pricing");
-		toolStrip.addButton(toolStripButton_pricing);
-
-		ToolStripSeparator toolStripSeparator_4 = new ToolStripSeparator();
-		toolStrip.addMember(toolStripSeparator_4);
-
-		ToolStripButton toolStripButton_signup = new ToolStripButton("Sign Up");
-		toolStrip.addButton(toolStripButton_signup);
-
-		ToolStripSeparator toolStripSeparator_2 = new ToolStripSeparator();
-		toolStrip.addMember(toolStripSeparator_2);
-
-		ToolStripButton toolStripButton = new ToolStripButton("Sign In");
-		toolStrip.addButton(toolStripButton);
-
-		ToolStripSeparator toolStripSeparator_5 = new ToolStripSeparator();
-		toolStrip.addMember(toolStripSeparator_5);
-
-		ToolStripSpacer toolStripSpacer = new ToolStripSpacer(110);
-		toolStrip.addSpacer(toolStripSpacer);
-
-		TextItem textItem_search = new TextItem("newTextItem_14", "Search");
-		toolStrip.addFormItem(textItem_search);
-		layout.addMember(toolStrip);
+		SectionStack sectionStack = new SectionStack();
+		layout.addMember(sectionStack);
 		return layout;
 	}
 
